0954a6cb55859e67ee3706d949f32e668b5b1a12
Basith <70204001+MuhammedBasith@users.noreply.github.com>
2023-12-27 00:07:37 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require('express');
+
+const genAI = new GoogleGenerativeAI(process.env.Gemini_API_Key);
+const app = express();
+const port = 3000;
+
+async function run(prompt) {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  
+    const finalPrompt = `${prompt} -- Correct the sentence grammatically`;
+    const result = await model.generateContent(finalPrompt);
+    const response = await result.response;
+    const text = response.text();
+
+    return text;
+}
+
+app.use(express.json());
+
+app.post('/correct', async (req, res) => {
+    console.log("Hit!")
+    const receivedText = req.body.text; 
+    console.log("Received text:", receivedText);
+    const correctedText = await run(receivedText);
+    res.json({ correctedText });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
